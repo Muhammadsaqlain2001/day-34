4197219cacd116181b776ef9ddbf82fb89942560
Muhammadsaqlain2001 <saqlainAsghar2001@gmail.com>
2024-06-11 00:38:23 -0700

    show rout meaning single post view and i manual create

--- restfull-appi/index.js
+++ restfull-appi/index.js
@@ -13,37 +13,44 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')))
 //post array
 let postArray = [
-{
-    userName : "Ali",
-    content : "I love Coding"
-},
-{
-    userName : "Ali",
-    content : "I love Coding"
-},
+    {
+        id: "1a",
+        userName: "Ali",
+        content: "I love Coding."
+    },
+    {
+        id: "2b",
+        userName: "Ali",
+        content: "Hard work is important to acheive success."
+    },
 
-{
-    userName : "Ali",
-    content : "I love Coding"
-}
+    {
+        id: "3c",
+        userName: "Muhammad Saqlain",
+        content: "I got selected for my 1st internship."
+    }
 ];
 // rout
 app.get('/', (req, res) => {
     res.send('server start well!')
 });
 
-app.get('/posts', (req, res)=>{
-    res.render('index.ejs', {postArray});
+app.get('/posts', (req, res) => {
+    res.render('index.ejs', { postArray});
 });
-app.get('/posts/new', (req, res)=>{
+app.get('/posts/new', (req, res) => {
     res.render('new.ejs')
 });
-app.post('/posts', (req,res)=>{
-    let {userName, content}= req.body;
-    postArray.push({userName, content});
+app.post('/posts', (req, res) => {
+    let { userName, content } = req.body;
+    postArray.push({ userName, content });
     res.redirect('/posts')
 });
-
+app.get('/posts/:id', (req, res) => {
+    let {id} = req.params;
+    let post = postArray.find((p)=>id === p.id)
+    res.render('show.ejs' , {post})
+})
 app.listen(port, () => {
     console.log('listening on port : 5000');
 })
