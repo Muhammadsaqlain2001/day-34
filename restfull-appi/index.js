42789004271f215e869e6214b2e548311be7e20f
Muhammadsaqlain2001 <saqlainAsghar2001@gmail.com>
2024-06-11 10:31:35 -0700

    get index root create

--- restfull-appi/index.js
+++ restfull-appi/index.js
@@ -4,16 +4,35 @@ const path = require('path');
 const port = 5000;
 
 //pars
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 //ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 //
-app.set(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
+//post array
+let postArray = [
+{
+    userName : "Ali",
+    content : "I love Coding"
+},
+{
+    userName : "Ali",
+    content : "I love Coding"
+},
 
-app.get('/', (req,res)=>{
+{
+    userName : "Ali",
+    content : "I love Coding"
+}
+];
+app.get('/', (req, res) => {
     res.send('server start well!')
-})
-app.listen(port, ()=>{
+});
+app.get('/posts', (req, res)=>{
+    res.render('index.ejs', {postArray})
+});
+app.listen(port, () => {
     console.log('listening on port : 5000');
 })
